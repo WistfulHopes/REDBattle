be4ce748daf4000afd4618997045605ba56b7b99
WistfulHopes <kp72002@gmail.com>
2025-01-31 20:50:49 -0800

    updated image pac

--- src/AALib/Base/sys_Math.h
+++ src/AALib/Base/sys_Math.h
@@ -20,13 +20,13 @@ namespace EAxis
 class AA_LinearColor
 {
 public:
-    float R, G, B, A {};
+    float R, G, B, A{};
 };
 
 class AA_Vector2
 {
 public:
-    float X, Y {};
+    float X, Y{};
 
     AA_Vector2()
     {
@@ -55,7 +55,7 @@ public:
 class AA_Vector3
 {
 public:
-    float X, Y, Z {};
+    float X, Y, Z{};
 
     AA_Vector3()
     {
@@ -109,7 +109,8 @@ public:
         return *this;
     }
 
-    AA_Vector3& operator*=(float V) {
+    AA_Vector3& operator*=(float V)
+    {
         X *= V;
         Y *= V;
         Z *= V;
@@ -148,7 +149,7 @@ public:
             float length = sqrtf(SquareSum);
             if (length != 0.0f)
             {
-                float ilength = 1.0f/length;
+                float ilength = 1.0f / length;
 
                 X *= ilength;
                 Y *= ilength;
@@ -175,11 +176,11 @@ public:
         }
 
         AA_Vector3 ret = AA_Vector3(0, 0, 0);
-        
+
         float length = sqrtf(SquareSum);
         if (length != 0.0f)
         {
-            float ilength = 1.0f/length;
+            float ilength = 1.0f / length;
             ret *= ilength;
         }
 
@@ -198,7 +199,7 @@ public:
 class AA_Vector4
 {
 public:
-    float X, Y, Z, W {};
+    float X, Y, Z, W{};
 
     AA_Vector4()
     {
@@ -255,11 +256,13 @@ public:
 class AA_Rotator
 {
 public:
-    float Pitch {};
-    float Yaw {};
-    float Roll {};
+    float Pitch{};
+    float Yaw{};
+    float Roll{};
 
-    AA_Rotator(float InPitch, float InYaw, float InRoll) : Pitch(InPitch), Yaw(InYaw), Roll(InRoll) {}
+    AA_Rotator(float InPitch, float InYaw, float InRoll) : Pitch(InPitch), Yaw(InYaw), Roll(InRoll)
+    {
+    }
 };
 
 class AA_Matrix
@@ -267,7 +270,7 @@ class AA_Matrix
 public:
     union
     {
-        float M[4][4] {};
+        float M[4][4]{};
     };
 
     AA_Matrix()
@@ -331,11 +334,13 @@ public:
         M[3][3] = 1;
     }
 
-    
+
     AA_Matrix(const AA_Vector3 EyePosition, const AA_Vector3 LookDirection, const AA_Vector3 UpVector)
     {
         // Vector3Subtract(eye, target)
-        AA_Vector3 vz = { EyePosition.X - LookDirection.X, EyePosition.Y - LookDirection.Y, EyePosition.Z - LookDirection.Z };
+        AA_Vector3 vz = {
+            EyePosition.X - LookDirection.X, EyePosition.Y - LookDirection.Y, EyePosition.Z - LookDirection.Z
+        };
 
         // Vector3Normalize(vz)
         AA_Vector3 v = vz;
@@ -343,19 +348,22 @@ public:
         if (length == 0.0f) length = 1.0f;
         float ilength = 1.0f / length;
         vz *= ilength;
-        
+
         // Vector3CrossProduct(up, vz)
-        AA_Vector3 vx = { UpVector.Y*vz.Z - UpVector.Z*vz.Y, UpVector.Z*vz.X - UpVector.X*vz.Z, UpVector.X*vz.Y - UpVector.Y*vz.X };
+        AA_Vector3 vx = {
+            UpVector.Y * vz.Z - UpVector.Z * vz.Y, UpVector.Z * vz.X - UpVector.X * vz.Z,
+            UpVector.X * vz.Y - UpVector.Y * vz.X
+        };
 
         // Vector3Normalize(x)
         v = vx;
-        length = sqrtf(v.X*v.X + v.Y*v.Y + v.Z*v.Z);
+        length = sqrtf(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
         if (length == 0.0f) length = 1.0f;
-        ilength = 1.0f/length;
+        ilength = 1.0f / length;
         vx *= ilength;
 
         // Vector3CrossProduct(vz, vx)
-        AA_Vector3 vy = { vz.Y*vx.Z - vz.Z*vx.Y, vz.Z*vx.X - vz.X*vx.Z, vz.X*vx.Y - vz.Y*vx.X };
+        AA_Vector3 vy = {vz.Y * vx.Z - vz.Z * vx.Y, vz.Z * vx.X - vz.X * vx.Z, vz.X * vx.Y - vz.Y * vx.X};
 
         M[0][0] = vx.X;
         M[0][1] = vy.X;
@@ -369,17 +377,17 @@ public:
         M[2][1] = vy.Z;
         M[2][2] = vz.Z;
         M[2][3] = 0.0f;
-        M[3][0] = -(vx.X*EyePosition.X+ vx.Y*EyePosition.Y + vx.Z*EyePosition.Z);   // Vector3DotProduct(vx, eye)
-        M[3][1] = -(vy.X*EyePosition.X+ vy.Y*EyePosition.Y + vy.Z*EyePosition.Z);   // Vector3DotProduct(vy, eye)
-        M[3][2] = -(vz.X*EyePosition.X+ vz.Y*EyePosition.Y + vz.Z*EyePosition.Z);   // Vector3DotProduct(vz, eye)
+        M[3][0] = -(vx.X * EyePosition.X + vx.Y * EyePosition.Y + vx.Z * EyePosition.Z); // Vector3DotProduct(vx, eye)
+        M[3][1] = -(vy.X * EyePosition.X + vy.Y * EyePosition.Y + vy.Z * EyePosition.Z); // Vector3DotProduct(vy, eye)
+        M[3][2] = -(vz.X * EyePosition.X + vz.Y * EyePosition.Y + vz.Z * EyePosition.Z); // Vector3DotProduct(vz, eye)
         M[3][3] = 1.0f;
     }
 
     AA_Matrix(float FOVY, float AspectRatio, float MinZ, float MaxZ)
     {
-        double top = MinZ*tan(FOVY*0.5);
+        double top = MinZ * tan(FOVY * 0.5);
         double bottom = -top;
-        double right = top*AspectRatio;
+        double right = top * AspectRatio;
         double left = -right;
 
         // MatrixFrustum(-right, right, -top, top, near, far);
@@ -387,20 +395,21 @@ public:
         float tb = (float)(top - bottom);
         float fn = MaxZ - MinZ;
 
-        M[0][0] = MinZ*2.0f/rl;
-        M[1][1] = MinZ*2.0f/tb;
-        M[2][0] = ((float)right + (float)left)/rl;
-        M[2][1] = ((float)top + (float)bottom)/tb;
-        M[2][2] = -(MaxZ + MinZ)/fn;
+        M[0][0] = MinZ * 2.0f / rl;
+        M[1][1] = MinZ * 2.0f / tb;
+        M[2][0] = ((float)right + (float)left) / rl;
+        M[2][1] = ((float)top + (float)bottom) / tb;
+        M[2][2] = -(MaxZ + MinZ) / fn;
         M[2][3] = -1.0f;
-        M[3][2] = -(MaxZ*MinZ*2.0f)/fn;
+        M[3][2] = -(MaxZ * MinZ * 2.0f) / fn;
     }
 
     AA_Matrix(AA_Rotator Rotator)
     {
-        Rotator = AA_Rotator(Rotator.Pitch * (M_PI/180.0f), Rotator.Yaw * (M_PI/180.0f), Rotator.Roll * (M_PI/180.0f));
+        Rotator = AA_Rotator(Rotator.Pitch * (M_PI / 180.0f), Rotator.Yaw * (M_PI / 180.0f),
+                             Rotator.Roll * (M_PI / 180.0f));
 
-        auto matrix = AA_Matrix(); 
+        auto matrix = AA_Matrix();
 
         float cosz = cosf(-Rotator.Roll);
         float sinz = sinf(-Rotator.Roll);
@@ -409,19 +418,19 @@ public:
         float cosx = cosf(-Rotator.Pitch);
         float sinx = sinf(-Rotator.Pitch);
 
-        M[0][0] = cosz*cosy;
-        M[0][1] = cosz*siny*sinx - sinz*cosx;
-        M[0][2] = cosz*siny*cosx + sinz*sinx;
+        M[0][0] = cosz * cosy;
+        M[0][1] = cosz * siny * sinx - sinz * cosx;
+        M[0][2] = cosz * siny * cosx + sinz * sinx;
 
-        M[1][0] = sinz*cosy;
-        M[1][1] = sinz*siny*sinx + cosz*cosx;
-        M[1][2] = sinz*siny*cosx - cosz*sinx;
+        M[1][0] = sinz * cosy;
+        M[1][1] = sinz * siny * sinx + cosz * cosx;
+        M[1][2] = sinz * siny * cosx - cosz * sinx;
 
         M[2][0] = -siny;
-        M[2][1] = cosy*sinx;
-        M[2][2] = cosy*cosx;
-        
-        *this = matrix;        
+        M[2][1] = cosy * sinx;
+        M[2][2] = cosy * cosx;
+
+        *this = matrix;
     }
 
     void Set(AA_Vector4 a, AA_Vector4 b, AA_Vector4 c, AA_Vector4 d)
@@ -500,22 +509,38 @@ public:
     {
         AA_Matrix result = AA_Matrix();
 
-        result.M[0][0] = M[0][0]*Other.M[0][0] + M[0][1]*Other.M[1][0] + M[0][2]*Other.M[2][0] + M[0][3]*Other.M[3][0];
-        result.M[0][1] = M[0][0]*Other.M[0][1] + M[0][1]*Other.M[1][1] + M[0][2]*Other.M[2][1] + M[0][3]*Other.M[3][1];
-        result.M[0][2] = M[0][0]*Other.M[0][2] + M[0][1]*Other.M[1][2] + M[0][2]*Other.M[2][2] + M[0][3]*Other.M[3][2];
-        result.M[0][3] = M[0][0]*Other.M[0][3] + M[0][1]*Other.M[1][3] + M[0][2]*Other.M[0][1] + M[0][3]*Other.M[3][3];
-        result.M[1][0] = M[1][0]*Other.M[0][0] + M[1][1]*Other.M[1][0] + M[1][2]*Other.M[2][0] + M[1][3]*Other.M[3][0];
-        result.M[1][1] = M[1][0]*Other.M[0][1] + M[1][1]*Other.M[1][1] + M[1][2]*Other.M[2][1] + M[1][3]*Other.M[3][1];
-        result.M[1][2] = M[1][0]*Other.M[0][2] + M[1][1]*Other.M[1][2] + M[1][2]*Other.M[2][2] + M[1][3]*Other.M[3][2];
-        result.M[1][3] = M[1][0]*Other.M[0][3] + M[1][1]*Other.M[1][3] + M[1][2]*Other.M[2][3] + M[1][3]*Other.M[3][3];
-        result.M[2][0] = M[2][0]*Other.M[0][0] + M[2][1]*Other.M[1][0] + M[2][2]*Other.M[2][0] + M[2][3]*Other.M[3][0];
-        result.M[2][1] = M[2][0]*Other.M[0][1] + M[2][1]*Other.M[1][1] + M[2][2]*Other.M[2][1] + M[2][3]*Other.M[3][1];
-        result.M[2][2] = M[2][0]*Other.M[0][2] + M[2][1]*Other.M[1][2] + M[2][2]*Other.M[2][2] + M[2][3]*Other.M[3][2];
-        result.M[2][3] = M[2][0]*Other.M[0][3] + M[2][1]*Other.M[1][3] + M[2][2]*Other.M[2][3] + M[2][3]*Other.M[3][3];
-        result.M[3][0] = M[3][0]*Other.M[0][0] + M[3][1]*Other.M[1][0] + M[3][2]*Other.M[2][0] + M[3][3]*Other.M[3][0];
-        result.M[3][1] = M[3][0]*Other.M[0][1] + M[3][1]*Other.M[1][1] + M[3][2]*Other.M[2][1] + M[3][3]*Other.M[3][1];
-        result.M[3][2] = M[3][0]*Other.M[0][2] + M[3][1]*Other.M[1][2] + M[3][2]*Other.M[2][2] + M[3][3]*Other.M[3][2];
-        result.M[3][3] = M[3][0]*Other.M[0][3] + M[3][1]*Other.M[1][3] + M[3][2]*Other.M[2][3] + M[3][3]*Other.M[3][3];
+        result.M[0][0] = M[0][0] * Other.M[0][0] + M[0][1] * Other.M[1][0] + M[0][2] * Other.M[2][0] + M[0][3] * Other.M
+            [3][0];
+        result.M[0][1] = M[0][0] * Other.M[0][1] + M[0][1] * Other.M[1][1] + M[0][2] * Other.M[2][1] + M[0][3] * Other.M
+            [3][1];
+        result.M[0][2] = M[0][0] * Other.M[0][2] + M[0][1] * Other.M[1][2] + M[0][2] * Other.M[2][2] + M[0][3] * Other.M
+            [3][2];
+        result.M[0][3] = M[0][0] * Other.M[0][3] + M[0][1] * Other.M[1][3] + M[0][2] * Other.M[0][1] + M[0][3] * Other.M
+            [3][3];
+        result.M[1][0] = M[1][0] * Other.M[0][0] + M[1][1] * Other.M[1][0] + M[1][2] * Other.M[2][0] + M[1][3] * Other.M
+            [3][0];
+        result.M[1][1] = M[1][0] * Other.M[0][1] + M[1][1] * Other.M[1][1] + M[1][2] * Other.M[2][1] + M[1][3] * Other.M
+            [3][1];
+        result.M[1][2] = M[1][0] * Other.M[0][2] + M[1][1] * Other.M[1][2] + M[1][2] * Other.M[2][2] + M[1][3] * Other.M
+            [3][2];
+        result.M[1][3] = M[1][0] * Other.M[0][3] + M[1][1] * Other.M[1][3] + M[1][2] * Other.M[2][3] + M[1][3] * Other.M
+            [3][3];
+        result.M[2][0] = M[2][0] * Other.M[0][0] + M[2][1] * Other.M[1][0] + M[2][2] * Other.M[2][0] + M[2][3] * Other.M
+            [3][0];
+        result.M[2][1] = M[2][0] * Other.M[0][1] + M[2][1] * Other.M[1][1] + M[2][2] * Other.M[2][1] + M[2][3] * Other.M
+            [3][1];
+        result.M[2][2] = M[2][0] * Other.M[0][2] + M[2][1] * Other.M[1][2] + M[2][2] * Other.M[2][2] + M[2][3] * Other.M
+            [3][2];
+        result.M[2][3] = M[2][0] * Other.M[0][3] + M[2][1] * Other.M[1][3] + M[2][2] * Other.M[2][3] + M[2][3] * Other.M
+            [3][3];
+        result.M[3][0] = M[3][0] * Other.M[0][0] + M[3][1] * Other.M[1][0] + M[3][2] * Other.M[2][0] + M[3][3] * Other.M
+            [3][0];
+        result.M[3][1] = M[3][0] * Other.M[0][1] + M[3][1] * Other.M[1][1] + M[3][2] * Other.M[2][1] + M[3][3] * Other.M
+            [3][1];
+        result.M[3][2] = M[3][0] * Other.M[0][2] + M[3][1] * Other.M[1][2] + M[3][2] * Other.M[2][2] + M[3][3] * Other.M
+            [3][2];
+        result.M[3][3] = M[3][0] * Other.M[0][3] + M[3][1] * Other.M[1][3] + M[3][2] * Other.M[2][3] + M[3][3] * Other.M
+            [3][3];
 
         return result;
     }
@@ -530,38 +555,38 @@ public:
         float a20 = M[2][0], a21 = M[2][1], a22 = M[2][2], a23 = M[2][3];
         float a30 = M[3][0], a31 = M[3][1], a32 = M[3][2], a33 = M[3][3];
 
-        float b00 = a00*a11 - a01*a10;
-        float b01 = a00*a12 - a02*a10;
-        float b02 = a00*a13 - a03*a10;
-        float b03 = a01*a12 - a02*a11;
-        float b04 = a01*a13 - a03*a11;
-        float b05 = a02*a13 - a03*a12;
-        float b06 = a20*a31 - a21*a30;
-        float b07 = a20*a32 - a22*a30;
-        float b08 = a20*a33 - a23*a30;
-        float b09 = a21*a32 - a22*a31;
-        float b10 = a21*a33 - a23*a31;
-        float b11 = a22*a33 - a23*a32;
+        float b00 = a00 * a11 - a01 * a10;
+        float b01 = a00 * a12 - a02 * a10;
+        float b02 = a00 * a13 - a03 * a10;
+        float b03 = a01 * a12 - a02 * a11;
+        float b04 = a01 * a13 - a03 * a11;
+        float b05 = a02 * a13 - a03 * a12;
+        float b06 = a20 * a31 - a21 * a30;
+        float b07 = a20 * a32 - a22 * a30;
+        float b08 = a20 * a33 - a23 * a30;
+        float b09 = a21 * a32 - a22 * a31;
+        float b10 = a21 * a33 - a23 * a31;
+        float b11 = a22 * a33 - a23 * a32;
 
         // Calculate the invert determinant (inlined to avoid double-caching)
-        float invDet = 1.0f/(b00*b11 - b01*b10 + b02*b09 + b03*b08 - b04*b07 + b05*b06);
+        float invDet = 1.0f / (b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06);
 
-        result.M[0][0] = (a11*b11 - a12*b10 + a13*b09)*invDet;
-        result.M[0][1] = (-a01*b11 + a02*b10 - a03*b09)*invDet;
-        result.M[0][2] = (a31*b05 - a32*b04 + a33*b03)*invDet;
-        result.M[0][3] = (-a21*b05 + a22*b04 - a23*b03)*invDet;
-        result.M[1][0] = (-a10*b11 + a12*b08 - a13*b07)*invDet;
-        result.M[1][1] = (a00*b11 - a02*b08 + a03*b07)*invDet;
-        result.M[1][2] = (-a30*b05 + a32*b02 - a33*b01)*invDet;
-        result.M[1][3] = (a20*b05 - a22*b02 + a23*b01)*invDet;
-        result.M[2][0] = (a10*b10 - a11*b08 + a13*b06)*invDet;
-        result.M[2][1] = (-a00*b10 + a01*b08 - a03*b06)*invDet;
-        result.M[2][2] = (a30*b04 - a31*b02 + a33*b00)*invDet;
-        result.M[2][3] = (-a20*b04 + a21*b02 - a23*b00)*invDet;
-        result.M[3][0] = (-a10*b09 + a11*b07 - a12*b06)*invDet;
-        result.M[3][1] = (a00*b09 - a01*b07 + a02*b06)*invDet;
-        result.M[3][2] = (-a30*b03 + a31*b01 - a32*b00)*invDet;
-        result.M[3][3] = (a20*b03 - a21*b01 + a22*b00)*invDet;
+        result.M[0][0] = (a11 * b11 - a12 * b10 + a13 * b09) * invDet;
+        result.M[0][1] = (-a01 * b11 + a02 * b10 - a03 * b09) * invDet;
+        result.M[0][2] = (a31 * b05 - a32 * b04 + a33 * b03) * invDet;
+        result.M[0][3] = (-a21 * b05 + a22 * b04 - a23 * b03) * invDet;
+        result.M[1][0] = (-a10 * b11 + a12 * b08 - a13 * b07) * invDet;
+        result.M[1][1] = (a00 * b11 - a02 * b08 + a03 * b07) * invDet;
+        result.M[1][2] = (-a30 * b05 + a32 * b02 - a33 * b01) * invDet;
+        result.M[1][3] = (a20 * b05 - a22 * b02 + a23 * b01) * invDet;
+        result.M[2][0] = (a10 * b10 - a11 * b08 + a13 * b06) * invDet;
+        result.M[2][1] = (-a00 * b10 + a01 * b08 - a03 * b06) * invDet;
+        result.M[2][2] = (a30 * b04 - a31 * b02 + a33 * b00) * invDet;
+        result.M[2][3] = (-a20 * b04 + a21 * b02 - a23 * b00) * invDet;
+        result.M[3][0] = (-a10 * b09 + a11 * b07 - a12 * b06) * invDet;
+        result.M[3][1] = (a00 * b09 - a01 * b07 + a02 * b06) * invDet;
+        result.M[3][2] = (-a30 * b03 + a31 * b01 - a32 * b00) * invDet;
+        result.M[3][3] = (a20 * b03 - a21 * b01 + a22 * b00) * invDet;
 
         return result;
     }
@@ -569,17 +594,17 @@ public:
     AA_Vector4 TransformFVector4(const AA_Vector4& P) const
     {
         AA_Vector4 result;
-        
+
         float x = P.X;
         float y = P.Y;
         float z = P.Z;
         float w = P.W;
 
-        result.X = M[0][0]*x + M[1][0]*y + M[2][0]*z +M[3][0]*w;
-        result.Y = M[0][1]*x + M[1][1]*y + M[2][1]*z +M[3][1]*w;
-        result.Z = M[0][2]*x + M[1][2]*y + M[2][2]*z +M[3][2]*w;
-        result.W = M[0][3]*x + M[1][3]*y + M[2][3]*z +M[3][3]*w;
-        
+        result.X = M[0][0] * x + M[1][0] * y + M[2][0] * z + M[3][0] * w;
+        result.Y = M[0][1] * x + M[1][1] * y + M[2][1] * z + M[3][1] * w;
+        result.Z = M[0][2] * x + M[1][2] * y + M[2][2] * z + M[3][2] * w;
+        result.W = M[0][3] * x + M[1][3] * y + M[2][3] * z + M[3][3] * w;
+
         return result;
     }
 
